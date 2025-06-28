f24b290830ec0f600d615c4081d17eb8cb53d430
krishramoju2 <futureai.incorporated@gmail.com>
2025-06-29 01:22:22 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+# Placeholder for language translation
+def translate_input(message, lang):
+    # Simulate translation (plug into Google Translate API or DeepL later)
+    return message
+
+def translate_output(message, lang):
+    return message
