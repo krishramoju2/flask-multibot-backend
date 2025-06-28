ca47c6f132dd20a6209d960a2373775142ab3c65
krishramoju2 <futureai.incorporated@gmail.com>
2025-06-29 03:34:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,1 @@
-from deep_translator import GoogleTranslator
 
-def translate_input(message, lang):
-    if lang == "en":
-        return message
-    try:
-        return GoogleTranslator(source=lang, target='en').translate(message)
-    except Exception:
-        return message
-
-def translate_output(message, lang):
-    if lang == "en":
-        return message
-    try:
-        return GoogleTranslator(source='en', target=lang).translate(message)
-    except Exception:
-        return message
